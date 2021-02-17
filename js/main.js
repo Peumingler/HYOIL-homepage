504fd241a6276175200011a01ec69391f5ecf828
김태영 <taeyeong@taeyeong-MacBook.local>
2021-02-17 15:28:00 +0900

    ADD: hyoil complete & menu auto scroll complete.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,14 @@
-import gethtml from './ajax.js';
-
-//쿼리스트링 파싱을 위한 함수
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-/*라우팅*/
-let routes = {
-    '/': { template: `<div style="z-index: -1;">
-    <video src="./static/videos/hyoil_intro_cn.mp4" type="video/ogg" poster="./static/images/image_bg.png" loop controls autoplay muted>브라우저가 지원하지 않는 기능입니다.</video>
-</div>` },
-    '/hyoil': { template: '' },
-    '/healo': { template: '' },
-    '/aieps': { template: '' },
-    '/klca': { template: '' },
-    '/contact': { template: '' }
-}
+import gethtml from "./ajax.js";
 
 window.onload = async function() {
-    //article 데이터 로드
+    //라우팅 template 로드
     for (const key in routes) {
         if (key === '/') { continue; } //root일 경우 패스
         routes[key].template = await gethtml(key.slice(1) + ".html");
     }
+
+
+    /*Vue 관련 코드들*/
     //사이드 메뉴 버튼
     const SideMenuBtn = Vue.createApp({
         data() {
@@ -74,4 +57,33 @@ window.onload = async function() {
             return Vue.h(this.ArticleViewComponent);
         }
     }).mount('#article');
+
+    moveToHash();
+}
+
+/*라우팅*/
+let routes = {
+    '/': { template: `<div style="z-index: -1;">
+    <video src="./static/videos/hyoil_intro_cn.mp4" type="video/ogg" poster="./static/images/image_bg.png" loop controls autoplay muted>브라우저가 지원하지 않는 기능입니다.</video>
+</div>` },
+    '/hyoil': { template: '' },
+    '/healo': { template: '' },
+    '/aieps': { template: '' },
+    '/klca': { template: '' },
+    '/contact': { template: '' }
+}
+
+//쿼리스트링 파싱을 위한 함수
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function moveToHash() {
+    let hash = window.location.hash.substr(1);;
+    if (hash) {
+        document.getElementById(hash).scrollIntoView({ behavior: "smooth" });
+    }
 }
