d9b0e670d5043c87396d60673d8a738dab9d12a4
김태영 <taeyeong@taeyeong-MacBook.local>
2021-02-21 00:44:49 +0900

    MODIFY: init link.

--- js/routing.js
+++ js/routing.js
@@ -3,13 +3,13 @@ export default {
         // url에 언어에 대한 데이터가 없으면 리다이렉트
         if (getParameterByName('p') === "/" || getParameterByName('p') === "") {
             let userLanguage = navigator.language.slice(0, 2); //ko-KR -> ko
-            history.replaceState({}, null, "/?p=/" + userLanguage + "/");
+            history.replaceState({}, null, "./?p=/" + userLanguage + "/");
         } else if (getParameterByName('p').slice(1, 3) === "ko") {
 
         }
     },
     changeLanguage(language) {
-        window.location.href = "/?p=/" + language + "/";
+        window.location.href = "./?p=/" + language + "/";
     }
 }
 
